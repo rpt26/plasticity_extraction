3e56cff16dceb9a70264e9ca3c602229c035f4e7
JimmyCambo <jc682@cam.ac.uk>
2018-02-19 17:50:59 +0000

    changed example data

--- inputs.py
+++ inputs.py
@@ -6,20 +6,20 @@
 sample_modulus = 117 # GPa
 sample_poisson = 0.33 # unitless
 indenter_radius = 1 # mm
-exp_filename = 'Copper.csv' # experiemntal data filename as a string
+exp_filename = 'AnnealedCopper.csv' # experiemntal data filename as a string
 
 
 # define the constitutive law as detailed in docs
-model = 'Ludwick' 
+model = 'voce' 
 # first guess at material variables as defined by constitutive law
 # e.g. Ludwick-holloman [yield_stress, K, n]
-material_variables = [207.6, 431, 0.5]
+material_variables = [47, 455.6, 0.534]
 
 # optional parameters, will take default values if not defineds
 coeff_of_friction = 0.2
 
 # Mesh parameters
-seeds = 120
+seeds = 80
 
 # Number of cores for simulations, default num_cores = 1
 num_cores = 8
